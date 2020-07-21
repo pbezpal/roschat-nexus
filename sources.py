7a9d3f46d08aac6866b996e2be1f8924da27d14d
Shavlovskiy Sergey <shavlovskiy.sn@infotek.ru>
2020-07-21 18:44:06 +0300

    add script for create tar

---
+++ sources.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os,sys,requests
+import tarfile
+
+with tarfile.open(sys.argv[2] + '.src.tar.gz','w') as tarinfo:
+    for name in os.listdir('./'):
+        tarinfo.add(name, filter=lambda x: None if x.name.startswith('ormp_nexus') else x)
+tarinfo.close()
