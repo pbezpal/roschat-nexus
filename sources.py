2142d22e747afab52460af24468941ef4f848f71
Shavlovskiy Sergey <shavlovskiy.sn@infotek.ru>
2020-07-21 19:35:42 +0300

    fix compress source

--- sources.py
+++ sources.py
@@ -3,7 +3,7 @@
 import os,sys,requests
 import tarfile
 
-with tarfile.open(sys.argv[2] + '.src.tar.gz','w') as tarinfo:
+with tarfile.open(sys.argv[1] + '.src.tar.gz','w') as tarinfo:
     for name in os.listdir('./'):
         tarinfo.add(name, filter=lambda x: None if x.name.startswith('ormp_nexus') else x)
 tarinfo.close()
