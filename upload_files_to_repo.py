c316ab9d23714b85ed968d1af06cf3ab07f59dec
Shavlovskiy Sergey <shavlovskiy.sn@infotek.ru>
2020-07-21 14:29:37 +0300

    add script for upload files to repo

---
+++ upload_files_to_repo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os,sys,requests
+
+folders=[]
+auth = ('jenkins','Rosch@tBuild')
+
+for result in os.walk(sys.argv[1]):
+    folders.append(result)
+
+for address, dirs, files in folders:
+    for filename in files:
+        if not sys.argv[2] in 'roschat-client':
+            url = 'http://10.10.199.217:8080/repository/' + sys.argv[2] + '/' + sys.argv[3] + '/' + filename
+            response = requests.put(url, data=open(address + '/' + filename, 'rb'), auth=auth)
+        else:
+            url = 'http://10.10.199.217:8080/repository/' + sys.argv[2] + '/' + sys.argv[3] + '/' + address.replace(sys.argv[1] + '/','') + '/' + filename
+            response = requests.put(url, data=open(address + '/' + filename, 'rb'), auth=auth)
+               
