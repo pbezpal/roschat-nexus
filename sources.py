94b287198abfbed72d6aaf9a5a2179444d415f2d
root <root@localhost.localdomain>
2020-07-22 11:57:54 +0300

    bugfix source

--- sources.py
+++ sources.py
@@ -3,7 +3,12 @@
 import os,sys,requests
 import tarfile
 
-with tarfile.open(sys.argv[1] + '.src.tar.gz','w') as tarinfo:
+folder_source = 'source'
+
+if not os.path.isdir(folder_source):
+    os.mkdir(folder_source)
+
+with tarfile.open('source/' + sys.argv[1] + '.src.tar.gz','w') as tarinfo:
     for name in os.listdir('./'):
         tarinfo.add(name, filter=lambda x: None if x.name.startswith('ormp_nexus') else x)
 tarinfo.close()
--- upload_files_to_repo.py
+++ upload_files_to_repo.py
@@ -11,9 +11,8 @@ for result in os.walk(sys.argv[1]):
 for address, dirs, files in folders:
     for filename in files:
         if sys.argv[2] == 'roschat-sources':
-            if filename in 'src.tar.gz':
-               url = 'http://10.10.199.217:8080/repository/' + sys.argv[2] + '/' + sys.argv[3] + '/' + sys.argv[4] + '/' + filename
-               response = requests.put(url, data=open(address + '/' + filename, 'rb'), auth=auth)
+           url = 'http://10.10.199.217:8080/repository/' + sys.argv[2] + '/' + sys.argv[3] + '/' + sys.argv[4] + '/' + filename
+           response = requests.put(url, data=open(address + '/' + filename, 'rb'), auth=auth)
         elif not sys.argv[2] in 'roschat-client':
             url = 'http://10.10.199.217:8080/repository/' + sys.argv[2] + '/' + sys.argv[3] + '/' + filename
             response = requests.put(url, data=open(address + '/' + filename, 'rb'), auth=auth)
